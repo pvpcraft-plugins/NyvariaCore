d97531923aa2d4d7d79ce4bf7ab12e6e69cb55ab
captbunzo <captbunzo@gmail.com>
2014-01-31 18:06:27 +0000

    Minor pom changes.

--- src/main/java/net/nyvaria/nyvariacore/commands/FeedCommand.java
+++ src/main/java/net/nyvaria/nyvariacore/commands/FeedCommand.java
@@ -34,7 +34,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 
 /**
  * @author Paul Thompson
